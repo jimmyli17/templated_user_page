dae0123405bbfdd37269c3a8399769b9e8f868b8
Jimmy Li <li17.jimmy@gmail.com>
2015-07-25 15:22:05 +0000

    Add form to update users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,10 @@ class ApplicationController < ActionController::Base
   def show
     @scientist = Scientist.find_by_id(params['id'])
   end
+  
   def new
   end
+  
   def create
     s = Scientist.new
     s.caption = params['caption']
@@ -17,6 +19,23 @@ class ApplicationController < ActionController::Base
     s.location = params['location']
     s.value = params['value']
     s.save;
+    redirect_to "/templated_user_page/#{s.id}"
+  end
+  
+  def edit
+    @scientist = Scientist.find_by_id(params['id'])
+  end
+  
+  def update
+    s = Scientist.find_by_id(params['id'])
+    s.caption = params['caption']
+    s.photo = params['photo']
+    s.name = params['name']
+    s.year = params['year']
+    s.description = params['description']
+    s.location = params['location']
+    s.value = params['value']
+    s.save
     redirect_to "/templated_user_page/#{s.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/templated_user_page/:id' => 'application#show'
   get '/new_scientist' => 'application#new'
   get '/create_scientist' => 'application#create'
+  get '/templated_user_page/:id/edit' => 'application#edit'
+  get '/update_scientist/:id' => 'application#update'
 end
